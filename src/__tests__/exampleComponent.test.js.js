c33ff13270eb8c076943771957615216ae52d3d1
ZohraTouazi <zohra.touazi@upply.com>
2026-01-03 15:56:48 +0100

    Updating things

--- src/__tests__/exampleComponent.test.js.js
+++
@@ -1,3 +0,0 @@
-test('This is an example component test', () => {
-   console.log("I was run");
-});
--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,16 +1,16 @@
-import { expect } from '@wdio/globals'
-import LoginPage from '../pageobjects/login.page'
-import SecurePage from '../pageobjects/secure.page'
+import { expect, browser, $ } from '@wdio/globals'
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
-        await LoginPage.open()
+        await browser.url(`https://the-internet.herokuapp.com/login`)
 
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveText(
+        await $('#username').setValue('tomsmith')
+        await $('#password').setValue('SuperSecretPassword!')
+        await $('button[type="submit"]').click()
+
+        await expect($('#flash')).toBeExisting()
+        await expect($('#flash')).toHaveText(
             expect.stringContaining('You logged into a secure area!'))
     })
-    
 })
 
